cc923d028bd39699632ada8fc7ef68e765e1b631
Christopher Tavares <aratava82@gmail.com>
2024-02-07 11:48:25 -0300

    latest-0-0-1

--- tests/bulk_email_validation.py
+++ tests/bulk_email_validation.py
@@ -1,24 +1,10 @@
-from emailfy import validate_email, EmailNotValidError, ValidatedEmail
+from emailfy import validate_email, EmailNotValidError
 
-emails_to_validate = ["aratava82@gmail.com", "exe@exemplocom"]
+emails_to_validate = ["user@exemplo.com", "exe@exemplocom"]
 
 try:
     validated_emails = validate_email(emails_to_validate)
-
     print(validated_emails)
-    #for original_email, result in validated_emails.items():
-    #    if isinstance(result, str):
-    #        print(f"Original Email: {original_email}")
-    #        print(f"Validation Error: {result}")
-    #        print("Email is not valid")
-    #        print("=" * 30)
-    #    elif isinstance(result, ValidatedEmail):
-    #        print(f"Original Email: {original_email}")
-    #        print(f"Normalized Email: {result.normalized}")
-    #        print("Email is valid")
-    #        print("=" * 30)
-    #    else:
-    #        print(f"Unexpected result for {original_email}: {result}")
 
 except EmailNotValidError as e:
     print(f"Error details: {e.details}")
